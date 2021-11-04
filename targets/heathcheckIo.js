9167b2ef1d7c9ba30019cf21b0c94ab1bda325e5
Anas <muhammed.anas@namshi.com>
2021-11-04 10:58:42 +0400

    Added healthcheckio target for stackdriver push gateway

--- targets/heathcheckIo.js
+++ targets/heathcheckIo.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 const config = require('/src/config');
 const logger = require('lib-logger');
 
-
 const createConnection = (connectionConfig) => {
     return axios.create({
         ...connectionConfig,
@@ -15,7 +14,7 @@ const createConnection = (connectionConfig) => {
 
 const ping = async ({ interval, job, team }) => {
     //checkName should have team, job and interval
-    let checkName = `last_run-${team ? team : 'live'}-${job}-${interval}`;
+    const checkName = `last_run-${team ? team : 'live'}-${job}-${interval}`;
 
     try {
         logger.info(`Pinging healthcheck ping api: ${checkName}`, {
@@ -27,23 +26,11 @@ const ping = async ({ interval, job, team }) => {
             throw new Error(`interval OR job name missing ${checkName}`)
         }
 
-        let connection = createConnection(_.get(config, 'healthcheckio.checksApiConfig', {}))
+        const connection = createConnection(_.get(config, 'healthcheckio.checksApiConfig', {}))
+        const pingUrl = await getPingUrl(checkName);
 
-        let checkConfig = {
-            "name": checkName,
-            "timeout": _.get(config, `healthcheckio.alertConfig.${checkName}.timeOut`) || 86400, // default 24 hours
-            "grace": _.get(config, `healthcheckio.alertConfig.${checkName}.timeOut`) || 3600, // default 3 hours
-            "unique": ["name"]
-        }
-        let response = await connection.post('api/v1/checks/', checkConfig);
-        let pingUrl = _.get(response, 'data.ping_url', '');
-        if (!pingUrl) {
-            logger.error(`Failed to create/retrieve the check: ${checkName}`)
-            throw new Error(`Failed to ping healthcheck for ${checkName}`)
-        }
-
-        let pingResponse = await connection.post(pingUrl, `team: ${team}, job: ${job}, interval: ${interval}`);
-        logger.info(`Ping Response: ${pingResponse.data}`);
+        const pingResponse = await connection.post(pingUrl, `team: ${team}, job: ${job}, interval: ${interval}`);
+        logger.info(`Ping Response: ${pingResponse.data} for check: ${checkName}`);
     } catch (err) {
         logger.error(`Failed to ping healthcheck ping-api: ${checkName}`, {
             errorMessage: err.message,
@@ -52,6 +39,30 @@ const ping = async ({ interval, job, team }) => {
     }
 };
 
+let pingUrlCache = {};
+const getPingUrl = async (checkName) => {
+    if(pingUrlCache[checkName]){
+        return pingUrlCache[checkName];
+    }
+    const connection = createConnection(_.get(config, 'healthcheckio.checksApiConfig', {}))
+
+    const checkConfig = {
+        "name": checkName,
+        "timeout": _.get(config, `healthcheckio.alertConfig.${checkName}.timeOut`) || 86400, // default 24 hours
+        "grace": _.get(config, `healthcheckio.alertConfig.${checkName}.timeOut`) || 3600, // default 3 hours
+        "unique": ["name"]
+    }
+    const response = await connection.post('api/v1/checks/', checkConfig);
+    const pingUrl = _.get(response, 'data.ping_url', '');
+    if (!pingUrl) {
+        logger.error(`Failed to create/retrieve the check: ${checkName}`)
+        throw new Error(`Failed to ping healthcheck for ${checkName}`)
+    }
+
+    //Cache the result
+    pingUrlCache[checkName] = pingUrl;
+    return pingUrl;
+}
+
 
 module.exports = { ping }
-//ping({ job: 'my-test-job', interval: 'daily' }).catch(err => console.log)
